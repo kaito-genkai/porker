5c067fbfb69bcecdf628d96da039b3792df88c8e
ryota.nakayama <true.saori@gmail.com>
2020-02-06 10:26:42 +0000

    init

--- microposts/app/controllers/toppages_controller.rb
+++
@@ -1,8 +0,0 @@
-class ToppagesController < ApplicationController
-  def index
-    if logged_in?
-      @micropost = current_user.microposts.build
-      @microposts = current_user.microposts.order(id: :desc).page(params[:page])
-    end  
-  end
-end
